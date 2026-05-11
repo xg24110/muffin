76bc6dbbaac62c7283ba5d877106d65baaaa0f85
xg24110 <xgcanares@fit.edu.ph>
2026-05-11 18:07:56 +0800

    ACT 2 in TSA1, the multiplication table

---
+++ TSA1/MTable.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Multiplication Table</title>
+    <link rel="stylesheet" href="MTable.css">
+</head>
+<body>
+    <h1><center>Multiplication Table</center></h1>
+    <div class = "container">
+        <?php
+            for ($i = 0; $i <= 10; $i++) {
+                for ($j = 0; $j <= 10; $j++) {
+                    $val = ($i * $j);
+                    $color = ($i + $j) % 2 == 0 ? 'dark' : 'light';
+                    echo "<div class='cell $color'>$val</div>";
+                } 
+            }
+            
+        ?>
+    </div>
+</body>
+</html>
